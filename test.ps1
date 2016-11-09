2bb40a2889ef1782b5cb2f5b01180163754c2994
fzed51 <fzed51+git@gmail.com>
2016-11-09 17:33:48 +0100

    Mise en place de script de test

---
+++ test.ps1
@@ -0,0 +1,7 @@
+./Convert-SvgFontToPng.ps1 -svgFont 'svgfont.svg'
+
+./Convert-SvgFontToPng.ps1 -svgFont 'svgfont.svg' -Color '#000000'
+
+./Convert-SvgFontToPng.ps1 -svgFont 'svgfont.svg' -Size 512
+
+./Convert-SvgFontToPng.ps1 -svgFont 'svgfont.svg' -outDirectory './out'
